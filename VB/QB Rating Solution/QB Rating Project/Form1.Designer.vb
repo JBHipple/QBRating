0dcbd939a10bac32b8a01681112509066a0a87f2
Joshua Hipple <jbhipple@gmail.com>
2015-04-13 03:45:49 -0500

    First commit
    Created VB Application to take input of the applicable football passing
    stats and calculate the Passer Rating as output.  User may select which
    league to calculate for, and calculations will be made using that
    league's specific formula.

---
+++ VB/QB Rating Solution/QB Rating Project/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAttempts = New System.Windows.Forms.Label()
+        Me.txtAttempts = New System.Windows.Forms.TextBox()
+        Me.lblCompletions = New System.Windows.Forms.Label()
+        Me.txtCompletions = New System.Windows.Forms.TextBox()
+        Me.lblYards = New System.Windows.Forms.Label()
+        Me.txtYards = New System.Windows.Forms.TextBox()
+        Me.lblTDs = New System.Windows.Forms.Label()
+        Me.txtTDs = New System.Windows.Forms.TextBox()
+        Me.lblINTs = New System.Windows.Forms.Label()
+        Me.txtINTs = New System.Windows.Forms.TextBox()
+        Me.grpLeague = New System.Windows.Forms.GroupBox()
+        Me.rdoNCAA = New System.Windows.Forms.RadioButton()
+        Me.rdoNFL = New System.Windows.Forms.RadioButton()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblRating = New System.Windows.Forms.Label()
+        Me.grpLeague.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblAttempts
+        '
+        Me.lblAttempts.AutoSize = True
+        Me.lblAttempts.Location = New System.Drawing.Point(13, 16)
+        Me.lblAttempts.Name = "lblAttempts"
+        Me.lblAttempts.Size = New System.Drawing.Size(117, 17)
+        Me.lblAttempts.TabIndex = 0
+        Me.lblAttempts.Text = "Passing Attempts"
+        '
+        'txtAttempts
+        '
+        Me.txtAttempts.Location = New System.Drawing.Point(137, 13)
+        Me.txtAttempts.Name = "txtAttempts"
+        Me.txtAttempts.Size = New System.Drawing.Size(63, 22)
+        Me.txtAttempts.TabIndex = 1
+        '
+        'lblCompletions
+        '
+        Me.lblCompletions.AutoSize = True
+        Me.lblCompletions.Location = New System.Drawing.Point(45, 44)
+        Me.lblCompletions.Name = "lblCompletions"
+        Me.lblCompletions.Size = New System.Drawing.Size(85, 17)
+        Me.lblCompletions.TabIndex = 2
+        Me.lblCompletions.Text = "Completions"
+        '
+        'txtCompletions
+        '
+        Me.txtCompletions.Location = New System.Drawing.Point(137, 41)
+        Me.txtCompletions.Name = "txtCompletions"
+        Me.txtCompletions.Size = New System.Drawing.Size(63, 22)
+        Me.txtCompletions.TabIndex = 3
+        '
+        'lblYards
+        '
+        Me.lblYards.AutoSize = True
+        Me.lblYards.Location = New System.Drawing.Point(31, 72)
+        Me.lblYards.Name = "lblYards"
+        Me.lblYards.Size = New System.Drawing.Size(99, 17)
+        Me.lblYards.TabIndex = 4
+        Me.lblYards.Text = "Passing Yards"
+        '
+        'txtYards
+        '
+        Me.txtYards.Location = New System.Drawing.Point(137, 69)
+        Me.txtYards.Name = "txtYards"
+        Me.txtYards.Size = New System.Drawing.Size(63, 22)
+        Me.txtYards.TabIndex = 5
+        '
+        'lblTDs
+        '
+        Me.lblTDs.AutoSize = True
+        Me.lblTDs.Location = New System.Drawing.Point(42, 100)
+        Me.lblTDs.Name = "lblTDs"
+        Me.lblTDs.Size = New System.Drawing.Size(88, 17)
+        Me.lblTDs.TabIndex = 6
+        Me.lblTDs.Text = "Touchdowns"
+        '
+        'txtTDs
+        '
+        Me.txtTDs.Location = New System.Drawing.Point(137, 97)
+        Me.txtTDs.Name = "txtTDs"
+        Me.txtTDs.Size = New System.Drawing.Size(63, 22)
+        Me.txtTDs.TabIndex = 7
+        '
+        'lblINTs
+        '
+        Me.lblINTs.AutoSize = True
+        Me.lblINTs.Location = New System.Drawing.Point(41, 129)
+        Me.lblINTs.Name = "lblINTs"
+        Me.lblINTs.Size = New System.Drawing.Size(89, 17)
+        Me.lblINTs.TabIndex = 8
+        Me.lblINTs.Text = "Interceptions"
+        '
+        'txtINTs
+        '
+        Me.txtINTs.Location = New System.Drawing.Point(137, 126)
+        Me.txtINTs.Name = "txtINTs"
+        Me.txtINTs.Size = New System.Drawing.Size(63, 22)
+        Me.txtINTs.TabIndex = 9
+        '
+        'grpLeague
+        '
+        Me.grpLeague.Controls.Add(Me.rdoNCAA)
+        Me.grpLeague.Controls.Add(Me.rdoNFL)
+        Me.grpLeague.Location = New System.Drawing.Point(226, 13)
+        Me.grpLeague.Name = "grpLeague"
+        Me.grpLeague.Size = New System.Drawing.Size(151, 86)
+        Me.grpLeague.TabIndex = 10
+        Me.grpLeague.TabStop = False
+        Me.grpLeague.Text = "Choose League:"
+        '
+        'rdoNCAA
+        '
+        Me.rdoNCAA.AutoSize = True
+        Me.rdoNCAA.Location = New System.Drawing.Point(7, 59)
+        Me.rdoNCAA.Name = "rdoNCAA"
+        Me.rdoNCAA.Size = New System.Drawing.Size(111, 21)
+        Me.rdoNCAA.TabIndex = 1
+        Me.rdoNCAA.TabStop = True
+        Me.rdoNCAA.Text = "NCAA Rating"
+        Me.rdoNCAA.UseVisualStyleBackColor = True
+        '
+        'rdoNFL
+        '
+        Me.rdoNFL.AutoSize = True
+        Me.rdoNFL.Location = New System.Drawing.Point(7, 31)
+        Me.rdoNFL.Name = "rdoNFL"
+        Me.rdoNFL.Size = New System.Drawing.Size(100, 21)
+        Me.rdoNFL.TabIndex = 0
+        Me.rdoNFL.TabStop = True
+        Me.rdoNFL.Text = "NFL Rating"
+        Me.rdoNFL.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(233, 105)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(135, 43)
+        Me.btnCalc.TabIndex = 11
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(95, 176)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 17)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Passer Rating:"
+        '
+        'lblRating
+        '
+        Me.lblRating.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRating.Location = New System.Drawing.Point(202, 173)
+        Me.lblRating.Name = "lblRating"
+        Me.lblRating.Size = New System.Drawing.Size(100, 23)
+        Me.lblRating.TabIndex = 13
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnCalc
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 216)
+        Me.Controls.Add(Me.lblRating)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.grpLeague)
+        Me.Controls.Add(Me.txtINTs)
+        Me.Controls.Add(Me.lblINTs)
+        Me.Controls.Add(Me.txtTDs)
+        Me.Controls.Add(Me.lblTDs)
+        Me.Controls.Add(Me.txtYards)
+        Me.Controls.Add(Me.lblYards)
+        Me.Controls.Add(Me.txtCompletions)
+        Me.Controls.Add(Me.lblCompletions)
+        Me.Controls.Add(Me.txtAttempts)
+        Me.Controls.Add(Me.lblAttempts)
+        Me.Name = "frmMain"
+        Me.Text = "QB Rating Calculator"
+        Me.grpLeague.ResumeLayout(False)
+        Me.grpLeague.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblAttempts As System.Windows.Forms.Label
+    Friend WithEvents txtAttempts As System.Windows.Forms.TextBox
+    Friend WithEvents lblCompletions As System.Windows.Forms.Label
+    Friend WithEvents txtCompletions As System.Windows.Forms.TextBox
+    Friend WithEvents lblYards As System.Windows.Forms.Label
+    Friend WithEvents txtYards As System.Windows.Forms.TextBox
+    Friend WithEvents lblTDs As System.Windows.Forms.Label
+    Friend WithEvents txtTDs As System.Windows.Forms.TextBox
+    Friend WithEvents lblINTs As System.Windows.Forms.Label
+    Friend WithEvents txtINTs As System.Windows.Forms.TextBox
+    Friend WithEvents grpLeague As System.Windows.Forms.GroupBox
+    Friend WithEvents rdoNCAA As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoNFL As System.Windows.Forms.RadioButton
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblRating As System.Windows.Forms.Label
+
+End Class
---
+++ VB/QB Rating Solution/QB Rating Project/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class frmMain
+
+    Private Sub CalcNFL()
+        ' Declare needed variables
+        Dim dblAttempts As Double
+        Dim dblCompletions As Double
+        Dim dblYards As Double
+        Dim dblTDs As Double
+        Dim dblINTs As Double
+        Dim dblA As Double
+        Dim dblB As Double
+        Dim dblC As Double
+        Dim dblD As Double
+        Dim dblRating As Double
+
+        ' Get input from text boxes
+        Double.TryParse(txtAttempts.Text, dblAttempts)
+        Double.TryParse(txtCompletions.Text, dblCompletions)
+        Double.TryParse(txtYards.Text, dblYards)
+        Double.TryParse(txtTDs.Text, dblTDs)
+        Double.TryParse(txtINTs.Text, dblINTs)
+
+        ' Calculate the sub parts of the formula
+        dblA = ((dblCompletions / dblAttempts) - 0.3) * 5.0
+        dblB = ((dblYards / dblAttempts) - 3) * 0.25
+        dblC = (dblTDs / dblAttempts) * 20.0
+        dblD = 2.375 - ((dblINTs / dblAttempts) * 25.0)
+
+        ' Check that sub parts fall in min/max of 0 to 2.375
+        If dblA < 0 Then
+            dblA = 0.0
+        End If
+        If dblA > 2.375 Then
+            dblA = 2.375
+        End If
+        If dblB < 0 Then
+            dblB = 0.0
+        End If
+        If dblB > 2.375 Then
+            dblB = 2.375
+        End If
+        If dblC < 0 Then
+            dblC = 0.0
+        End If
+        If dblC > 2.375 Then
+            dblC = 2.375
+        End If
+        If dblD < 0 Then
+            dblD = 0.0
+        End If
+        If dblD > 2.375 Then
+            dblD = 2.375
+        End If
+
+        ' Calculate the main formula
+        dblRating = ((dblA + dblB + dblC + dblD) / 6) * 100
+
+        ' Assign the rating to the rating label
+        lblRating.Text = dblRating.ToString("N1")
+    End Sub
+
+    Private Sub CalcNCAA()
+        ' Declare needed variables
+        Dim intAttempts As Integer
+        Dim intCompletions As Integer
+        Dim intYards As Integer
+        Dim intTDs As Integer
+        Dim intINTs As Integer
+        Dim dblRating As Double
+
+        ' Get input from text boxes
+        Integer.TryParse(txtAttempts.Text, intAttempts)
+        Integer.TryParse(txtCompletions.Text, intCompletions)
+        Integer.TryParse(txtYards.Text, intYards)
+        Integer.TryParse(txtTDs.Text, intTDs)
+        Integer.TryParse(txtINTs.Text, intINTs)
+
+        ' Calculate the rating
+        dblRating = ((8.4 * intYards) + (330 * intTDs) + (100 * intCompletions) - (200 * intINTs)) / intAttempts
+
+        ' Assign the rating to the rating label
+        lblRating.Text = dblRating.ToString("N1")
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Select NFL Rating as default
+        rdoNFL.Select()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Make sure text boxes contain data, set flag to false if input error
+        Dim blnValidInput As Boolean = True
+
+        If txtAttempts.Text = String.Empty Then
+            blnValidInput = False
+        End If
+        If txtCompletions.Text = String.Empty Then
+            blnValidInput = False
+        End If
+        If txtYards.Text = String.Empty Then
+            blnValidInput = False
+        End If
+        If txtTDs.Text = String.Empty Then
+            blnValidInput = False
+        End If
+        If txtINTs.Text = String.Empty Then
+            blnValidInput = False
+        End If
+
+        ' If flag is true, perform the calculation based on which radio button selected
+        ' If flag is false, show MessageBox informing user there is invalid input.
+        If blnValidInput Then
+            If rdoNFL.Checked Then
+                CalcNFL()
+            Else
+                CalcNCAA()
+            End If
+        Else
+            MessageBox.Show("Invalid input in text boxes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+        txtAttempts.Focus()
+    End Sub
+
+    Private Sub ClearLabels(sender As Object, e As EventArgs) Handles rdoNFL.CheckedChanged, rdoNCAA.CheckedChanged
+        ' Clear the text boxes and label when league is changed
+        txtAttempts.Text = String.Empty
+        txtCompletions.Text = String.Empty
+        txtYards.Text = String.Empty
+        txtTDs.Text = String.Empty
+        txtINTs.Text = String.Empty
+        lblRating.Text = String.Empty
+
+        txtAttempts.Focus()
+    End Sub
+
+    Private Sub txtAttempts_GotFocus(sender As Object, e As EventArgs) Handles txtAttempts.GotFocus, txtCompletions.GotFocus, txtYards.GotFocus, txtTDs.GotFocus, txtINTs.GotFocus
+        ' Set the text boxes to automaticall select all when focused on,
+        ' making rapid input of stats easier, while retaining input information
+        ' after calculation to manually verify input.
+        sender.SelectAll()
+    End Sub
+
+    Private Sub txtAttempts_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtAttempts.KeyPress, txtCompletions.KeyPress, txtYards.KeyPress, txtTDs.KeyPress, txtINTs.KeyPress
+        ' Limit input to numbers and backspace
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
